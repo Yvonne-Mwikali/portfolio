43ef1a3e6050aae1a4d64a57add3787aa2815dd9
Yvonne Mwikali <yvonnemwikali162@gmail.com>
2026-01-21 17:24:43 +0300

    Add new graphic assets and update resume; enhance project data in projects.tsx

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -19,6 +19,21 @@ const kazilogo = "../kazilogo.png";
 const berenlogo = "../berenlogo.png";
 const vwt = "../vwt.png";
 const port = "../port.png";
+const graphic6 = "../graphic6.png";
+const graphic7 = "../graphic7.png";
+const graphic8 = "../graphic8.png";
+const graphic9 = "../graphic9.png";
+const graphic10 = "../graphic10.png";
+const graphic11 = "../graphic11.png";
+const graphic12 = "../graphic12.png";
+const graphic13 = "../graphic13.png";
+const graphic14 = "../graphic14.png";
+const graphic15 = "../graphic15.png";
+const graphic16 = "../graphic16.png";
+const graphic17 = "../graphic17.png";
+const graphic18 = "../graphic18.png";
+const graphic19 = "../graphic19.png";
+const agc = "../agc.jpeg";
 
 export const projects: ProjectInterface[] = [
   {
@@ -33,7 +48,8 @@ export const projects: ProjectInterface[] = [
     title: "RentHub Website Version 2",
     category: "webs",
     image: rntv2,
-    description: "The second version of the RentHub website with improved features.",
+    description:
+      "The second version of the RentHub website with improved features.",
     liveLink: "https://renthub.kberen.com/",
   },
   {
@@ -41,7 +57,8 @@ export const projects: ProjectInterface[] = [
     title: "Task Management UI",
     category: "uiux",
     image: uiux,
-    description: "An intuitive user interface design for a task management application.",
+    description:
+      "An intuitive user interface design for a task management application.",
   },
   {
     id: 5,
@@ -55,7 +72,8 @@ export const projects: ProjectInterface[] = [
     title: "Portfolio Website",
     category: "webs",
     image: port,
-    description: "A responsive portfolio website showcasing creative works and projects.",
+    description:
+      "A responsive portfolio website showcasing creative works and projects.",
   },
   {
     id: 7,
@@ -107,5 +125,113 @@ export const projects: ProjectInterface[] = [
     category: "graphics",
     image: graphic5,
     description: "A creative design for digital advertising campaigns.",
+  },
+ 
+ 
+  {
+    id: 14,
+    title: "Graphic 6",
+    category: "graphics",
+    image: graphic6,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 15,
+    title: "Graphic 7",
+    category: "graphics",
+    image: graphic7,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 16,
+    title: "Graphic 8",
+    category: "graphics",
+    image: graphic8,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 17,
+    title: "Graphic 9",
+    category: "graphics",
+    image: graphic9,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 18,
+    title: "Graphic 10",
+    category: "graphics",
+    image: graphic10,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 19,
+    title: "Graphic 11",
+    category: "graphics",
+    image: graphic11,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 20,
+    title: "Graphic 12",
+    category: "graphics",
+    image: graphic12,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 21,
+    title: "Graphic 13",
+    category: "graphics",
+    image: graphic13,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 22,
+    title: "Graphic 14",
+    category: "graphics",
+    image: graphic14,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 23,
+    title: "Graphic 15",
+    category: "graphics",
+    image: graphic15,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 24,
+    title: "Graphic 16",
+    category: "graphics",
+    image: graphic16,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 25,
+    title: "Graphic 17",
+    category: "graphics",
+    image: graphic17,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 26,
+    title: "Graphic 18",
+    category: "graphics",
+    image: graphic18,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 27,
+    title: "Graphic 19",
+    category: "graphics",
+    image: graphic19,
+    description: "A creative design for digital advertising campaigns.",
+  },
+  {
+    id: 28,
+    title: "Agency Enterprise Application",
+    category: "webs",
+    image: agc,
+    description: "An agency application for managing enterprise resources.",
+    liveLink: "https://renthub-enterprise.kberen.com/",
   },
 ];
